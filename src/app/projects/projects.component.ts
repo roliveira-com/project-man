77c81630b385b3bb61d90dd407f6201b668b28d9
Rodrigo Oliveira <rodrigooliveira@rodrigo.local>
2017-11-21 08:43:36 -0200

    usando angular native animations na marcação de projetos (com bug)

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 import { Project } from './project.model';
 
@@ -8,12 +9,32 @@ import { ProjectsService } from './projects.service';
   selector: 'app-projects',
   templateUrl: './projects.component.html',
   styleUrls: ['./projects.component.css'],
+  animations: [
+    trigger('selectProj',[
+      state('unselected', style({
+        height: "65px",
+        border: "1px solid #CCC"
+      })),
+      state('selected', style({
+        height: "205px",
+        padding: ".8em",
+        border: "1px solid #00d1b2"
+      })),
+      transition('unselected => selected', [
+        style({
+          transform: "scale(1.05)"
+        }),
+        animate('600ms 100ms ease-out')
+      ])
+    ])
+  ]
 })
 export class ProjectsComponent implements OnInit {
   projects: Project[];
   markedPrjIndex = null;
   progress = 'progressing';
   createNew = false;
+  projectSelection = 'unselected';
 
   constructor(private prjService: ProjectsService) { }
 
@@ -29,6 +50,10 @@ export class ProjectsComponent implements OnInit {
 
   onStatusUpdated(newStatus: string, id: number) {
     this.projects[id].status = newStatus;
+  }
+
+  onSelect(){
+    this.projectSelection = 'selected';
   }
 
   onProjectDeleted(index: number) {
