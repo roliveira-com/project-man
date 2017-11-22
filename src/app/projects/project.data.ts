1b2753e9e34e7ea9962cbc431012eef5173e7b5c
Rodrigo Oliveira <rodrigooliveira@rodrigo.local>
2017-11-22 08:46:34 -0200

    backup de dados para uso offline

---
+++ src/app/projects/project.data.ts
@@ -0,0 +1,8 @@
+import { Project } from './project.model';
+
+export const projects: Project[] = [
+  { name: 'Learn Angular Styles', description: 'Practice hard to understand how you may style components and update styles dynamically', status: 'active' },
+  { name: 'Learn Angular Animations', description: 'Learn how Angular helps with animating elements on your page', status: 'active' },
+  { name: 'Understanding Angular Basics', description: 'Understand what Angular is, how it works and how and when you might use it', status: 'inactive' },
+  { name: 'Learn JavaScript, HTML and CSS', description: 'Absolutely required to dive deep into Angular and all its features', status: 'critical' },
+]
--- src/app/projects/projects.service.ts
+++ src/app/projects/projects.service.ts
@@ -10,13 +10,4 @@ export class ProjectsService {
     { name: 'Understanding Angular Basics', description: 'Understand what Angular is, how it works and how and when you might use it', status: 'inactive'},
     { name: 'Learn JavaScript, HTML and CSS', description: 'Absolutely required to dive deep into Angular and all its features', status: 'critical'},
   ];
-
-  loadProjects(): Observable<Project[]> {
-    const prjLoader = Observable.create((observer: Observer<Project[]>) => {
-      setTimeout(() => {
-        observer.next(this.projects);
-      }, 2000);
-    });
-    return prjLoader;
-  }
 }
